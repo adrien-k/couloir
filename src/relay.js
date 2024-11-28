c4115eb7a3ad88d5e9649ad7a71ded735fa2834a
Adrien Kiren <adrien.kiren@gmail.com>
2024-11-28 15:02:25 +0100

    Close couloir and disable http keepalive

--- src/relay.js
+++ src/relay.js
@@ -1,8 +1,9 @@
 import net from "node:net";
 
 export const OPEN_COULOIR = "OPEN_COULOIR";
+export const CLOSE_COULOIR = "CLOSE_COULOIR";
 const JOIN_COULOIR = "JOIN_COULOIR";
-const JOIN_COULOIR_MATCH = /^JOIN_COULOIR (.*)$/m;
+const COULOIR_MATCHER = /^[A-Z]+_COULOIR (.*)$/m;
 const HOST_MATCH = /\r\nHost: (.+)\r\n/;
 
 export default function relay(port, domain) {
@@ -11,19 +12,19 @@ export default function relay(port, domain) {
   const clients = {};
 
   function bindNextSockets(host) {
-    if (clients[host].length && hosts[host].length) {
+    while(clients[host].length && hosts[host].length) {
       const clientSocket = clients[host].shift();
       const hostSocket = hosts[host].shift();
       clientSocket.socket.pipe(hostSocket.socket);
       // Write the data that has already been consumed
       hostSocket.socket.write(clientSocket.request);
       hostSocket.socket.pipe(clientSocket.socket);
-    }
+    } 
   }
+
   let socketCounter = 0;
   const server = net.createServer((socket) => {
     socketCounter++;
-    console.log(`Client connected #${socketCounter}`);
 
     // A Relay socket can be either a regular client HTTP request or a host proxy socket.
     // They are stored in either `hosts` or `clients` hashes respectively and pulled by bindNextSockets
@@ -54,51 +55,63 @@ export default function relay(port, domain) {
         relaySocket.host = host;
         hosts[host] = [];
         clients[host] = [];
-        socket.write(`${host}\n`);
+        socket.write(`${host}\r\n`);
+        return;
+      }
 
-        socket.off("data", onSocketData);
+      if (dataString.startsWith(CLOSE_COULOIR)) {
+        const couloirHost = dataString.match(COULOIR_MATCHER)[1];
+        console.log(`Couloir host ${couloirHost} closed by #${relaySocket.id}`);
+        delete hosts[couloirHost]
+        delete clients[couloirHost]
+        socket.write(`OK\r\n`);
         return;
       }
 
       if (dataString.startsWith(JOIN_COULOIR)) {
-        const couloirHost = dataString.match(JOIN_COULOIR_MATCH)[1];
+        const host = dataString.match(COULOIR_MATCHER)[1];
+        relaySocket.host = host;
+        hosts[host].push(relaySocket);
         console.log(
-          `Added socket ${relaySocket.host}#${relaySocket.id} (total: ${hosts[couloirHost].length})`
+          `Host socket ${host}#${relaySocket.id} connected (${hosts[host].length})`
         );
 
-        relaySocket.host = couloirHost;
-        hosts[couloirHost].push(relaySocket);
-        bindNextSockets(couloirHost);
+        bindNextSockets(host);
 
         socket.off("data", onSocketData);
         return;
       }
 
-      // TOOD: CLOSE_COULOIR
-
       relaySocket.request += dataString;
+      // Wait end of headers
+      if (relaySocket.request.indexOf("\r\n\r\n") === -1) {
+        return
+      }
 
       const host_match = relaySocket.request.match(HOST_MATCH);
       if (host_match) {
         // This removes the potential port
         const host = new URL(`http://${host_match[1]}`).hostname;
         if (clients[host]) {
-          console.log(`Client socket ${relaySocket.host}#${relaySocket.id}`);
-
           relaySocket.host = host;
           clients[host].push(relaySocket);
+          console.log(`Client socket ${host}#${relaySocket.id} connected (${clients[host].length})`);
+
+          // Remove http 1.1 keep-alive behaviour to ensure the socket is quickly re-created for other client sockets
+          // and to avoid parsing headers of the follow-up request that may go through the same socket.
+          relaySocket.request = relaySocket.request.replace(/\r\nconnection:.*\r\n/i, "\r\n")
+          relaySocket.request = relaySocket.request.replace("\r\n\r\n", "\r\nConnection: close\r\n\r\n")
+
           bindNextSockets(host);
 
+          // No need to look further in request body
           socket.off("data", onSocketData);
           return;
         }
       }
 
-      // Reached end of Request headers with no matching HOST
-      if (relaySocket.request.indexOf("\r\n\r\n") > -1) {
-        socket.write("HTTP/1.1 404 Not found\r\n\r\nNot found");
-        socket.end();
-      }
+      socket.write("HTTP/1.1 404 Not found\r\n\r\nNot found");
+      socket.end();
     };
 
     socket.on("data", onSocketData);
