c57a4956aed47aefc03dcb7f8037772b20f4790c
adrien-k <adrien-k@users.noreply.github.com>
2024-11-28 15:02:25 +0100

    Close couloir and disable http keepalive

--- src/bind.js
+++ src/bind.js
@@ -1,42 +1,42 @@
-import net from 'node:net';
-import { OPEN_COULOIR} from './relay.js'
+import net from "node:net";
+import { OPEN_COULOIR, CLOSE_COULOIR } from "./relay.js";
 
+const PROXY_CONCURRENT = 1;
 
-const PROXY_CONCURRENT = 4;
-
-export default function bind(relayHost, port, { relayPort = 80} = {}) {
-  let socketCount = 0
-  let localSocketError = false
+export default async function bind(relayHost, port, { relayPort = 80 } = {}) {
+  let couloirHost
+  let socketCount = 0;
+  let localSocketError = false;
 
   async function openSockets(couloirHost) {
     if (localSocketError) {
-      return
+      return;
     }
 
     try {
       while (socketCount < PROXY_CONCURRENT) {
-        console.log(`opening relay socket (current: ${socketCount})`)
+        console.log(`opening relay socket (current: ${socketCount})`);
         // Increment before connection to avoid going over the limit while connection happens
         // as this function may be called concurrently
-        socketCount++ 
-        await connect(couloirHost)
+        socketCount++;
+        await connect(couloirHost);
       }
     } catch (err) {
-      socketCount--
+      socketCount--;
       if (!localSocketError) {
-        console.error(`Error connecting: ${err.message}.\nRetrying in 1s`)
-        localSocketError = true 
+        console.error(`Error connecting: ${err.message}.\nRetrying in 1s`);
+        localSocketError = true;
         setTimeout(() => {
-          localSocketError = false
-          openSockets(couloirHost)
-        }, 1000)
+          localSocketError = false;
+          openSockets(couloirHost);
+        }, 1000);
       }
     }
   }
 
   async function connect(couloirHost) {
     return new Promise((resolve, reject) => {
-      const localSocket = net.createConnection({ host: '127.0.0.1', port }, () => {
+      const localSocket = net.createConnection({ host: "127.0.0.1", port }, () => {
         const proxyHostSocket = net.createConnection({ host: relayHost, port: relayPort }, () => {
           proxyHostSocket.write(`JOIN_COULOIR ${couloirHost}\n`);
           localSocket.pipe(proxyHostSocket);
@@ -45,33 +45,49 @@ export default function bind(relayHost, port, { relayPort = 80} = {}) {
         });
 
         localSocket.on("end", () => {
-          proxyHostSocket.end();
-          socketCount--
+          console.log("localSocket end");
+          socketCount--;
           openSockets(couloirHost);
         });
+      });
 
-        proxyHostSocket.on("end", () => {
-          localSocket.end();
-        })  
-      })
-      
       localSocket.on("error", (err) => {
         reject(err);
       });
-    })
+    });
   }
 
-  const joinRequestSocket = net.createConnection({ host: relayHost, port: relayPort }, () => {
-    joinRequestSocket.on("data", async (data) => {
-      const couloirHost = data.toString();
-      console.log(`Couloir opened on ${new URL(`http://${couloirHost}:${relayPort}`)}`);
-      joinRequestSocket.end();
-      openSockets(couloirHost)
-    });
-    joinRequestSocket.write(OPEN_COULOIR);
-  });
+  async function sendMessage(message) {
+    return new Promise((resolve, reject) => {
+      const socket = net.createConnection({ host: relayHost, port: relayPort }, () => {
+        socket.on("data", (data) => {
+          resolve(data.toString());
+          socket.end();
+        });
+        socket.write(`${message}\r\n`);
+      });
 
-  joinRequestSocket.on("error", (err) => {
+      socket.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+
+  try {
+    couloirHost = await sendMessage(OPEN_COULOIR);
+    console.log(`Couloir opened on ${new URL(`http://${couloirHost}:${relayPort}`)}`);
+    openSockets(couloirHost);
+  } catch (e) {
     console.error(`Error connecting to relay server: ${err.message}`);
-  });
+    process.exit(1);
+  }
+
+  process.on("SIGTERM", cleanup);
+  process.on("SIGINT", cleanup);
+
+  async function cleanup() {
+    console.log("Cleaning up before exit...");
+    await sendMessage(`${CLOSE_COULOIR} ${couloirHost}`)
+    process.exit();
+  }
 }
